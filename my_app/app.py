8ca0c7853841045c7650b6d0bff91da6c16e8abc
Zain Khan <program01code@gmail.com>
2025-06-20 20:47:00 +0500

    Refactor: Move app to factory function
    - Converts the app to a factory function to enable configuration and
    testing. - Moves static folder to a relative path - Add pop route for
    clearing session data - Move templates to my_app dir

--- my_app/app.py
+++ my_app/app.py
@@ -7,11 +7,7 @@ from api_setup.chat_api import main_func
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-
-app = Flask(__name__, template_folder='templates', static_folder='static')
-
 load_dotenv()
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 # Form Classes:
 class ChatAI_Form(FlaskForm):
@@ -22,35 +18,58 @@ class ImageAI_Form(FlaskForm):
     prompt = StringField("ImagePrompt", validators=[DataRequired()])
     submit_imageAI = SubmitField('imageAI')
 
-# Page Routes
-@app.route('/')
-def main():
-    return redirect(url_for('home'))
+# Factory Function
+def create_app(test_config=None):
+    app = Flask(__name__, template_folder='templates', static_folder='../static', instance_relative_config=True)
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    chatAI_form = ChatAI_Form()
-    if 'prompt_list' not in session:
-        session['prompt_list'] = ['No Data Yet']
+    app.config.from_mapping(
+        SECRET_KEY = os.environ['SECRET_KEY'],
+    )
 
-    script_prompt = "Hello World!"
-    chat_reply = "No AI response yet. Type a prompt to get started!"
-
-    if request.method == 'POST':
-        if chatAI_form.validate_on_submit():
-            script_prompt = chatAI_form.prompt.data
-            flash(f'Sent prompt: "{script_prompt}" to the AI.', 'success')
-            (chat_reply, promptList) = main_func(script_prompt)
-            session['prompt_list'] = promptList
-            #return redirect(url_for('home'))
-        else:
-            flash('Chat Prompt validation failed. Please enter something.', 'danger')
-            chat_reply = "Please fix the errors in your prompt."
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
     else:
-        chat_reply = script_prompt
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-    return render_template("home.html", chatAI_form=chatAI_form, chat_reply=chat_reply, prompt_list=session.get('prompt_list'))
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
+    # Page Routes
+    @app.route('/')
+    def main():
+        return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    @app.route('/pop')
+    def pop():
+        session.pop('prompt_list', None)
+        return redirect(url_for('home'))
+
+    @app.route('/home', methods=['GET', 'POST'])
+    def home():
+        chatAI_form = ChatAI_Form()
+        if 'prompt_list' not in session:
+            session['prompt_list'] = ['No Data Yet']
+
+        script_prompt = "Hello World!"
+        chat_reply = "No AI response yet. Type a prompt to get started!"
+
+        if request.method == 'POST':
+            if chatAI_form.validate_on_submit():
+                script_prompt = chatAI_form.prompt.data
+                flash(f'Sent prompt: "{script_prompt}" to the AI.', 'success')
+                (chat_reply, promptList) = main_func(script_prompt)
+                session['prompt_list'] = promptList
+                #return redirect(url_for('home'))
+            else:
+                flash('Chat Prompt validation failed. Please enter something.', 'danger')
+                chat_reply = "Please fix the errors in your prompt."
+        else:
+            chat_reply = script_prompt
+
+        return render_template("home.html", chatAI_form=chatAI_form, chat_reply=chat_reply, prompt_list=session.get('prompt_list'))
+
+    return app
